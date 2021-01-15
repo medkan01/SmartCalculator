9e472e902bbaf0f436bfd5a3a0c4183fbdbb72cb
medkan01 <mehdiakniou@gmail.com>
2021-01-15 18:06:58 +0100

    raw code to functions

--- src/model/model.py
+++ src/model/model.py
@@ -1,17 +1,23 @@
 import speech_recognition as sr
 
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Réglage du bruit ambiant... Patientez...")
-    r.adjust_for_ambient_noise(source)
-    print("Dites quelque chose")
-    audio = r.listen(source)
+def getAudio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Réglage du bruit ambiant... Patientez...")
+        r.adjust_for_ambient_noise(source)
+        print("Dites quelque chose")
+        audio = r.listen(source)
+        return audio
 
-try:
-    text = r.recognize_google(audio, language="fr-FR")
-    print("Vous avez dit : " + text)
-except sr.UnknownValueError:
-    print("L'audio n'a pas été compris")
-except sr.RequestError as e:
-    print("Le service Google Speech API ne fonctionne plus" + format(e))
+def getTextFromAudio(audio, language_audio):
+    r = sr.Recognizer()
+    try:
+        text = r.recognize_google(audio, language=language_audio)
+        return text
+    except sr.UnknownValueError:
+        print("L'audio n'a pas été compris")
+    except sr.RequestError as e:
+        print("Le service Google Speech API ne fonctionne plus" + format(e))
+
+print(getTextFromAudio(getAudio(), "fr-FR"))
